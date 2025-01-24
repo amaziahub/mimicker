8700f69ff0b8807497c900f8fad6cb460811408c
Luc Sorel-Giffo <luc.sorel@gmail.com>
2025-01-24 14:25:56 +0100

    feat(server): make mimicker able to run several times in a row so that tests can be run multiple times in a row

--- mimicker/server.py
+++ mimicker/server.py
@@ -7,11 +7,13 @@ from mimicker.handler import MimickerHandler
 from mimicker.route import Route
 from mimicker.stub_group import StubGroup
 
+class ReusableAddressTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
 
 class MimickerServer:
     def __init__(self, port: int = 8080):
         self.stub_matcher = StubGroup()
-        self.server = socketserver.TCPServer(("", port), self._handler_factory)
+        self.server = ReusableAddressTCPServer(("", port), self._handler_factory)
         self._thread = threading.Thread(target=self.server.serve_forever, daemon=True)
         atexit.register(self.shutdown)
 
