5c6fc0d1279922d01a88586c7f8d9bd486831bba
Luc Sorel-Giffo <luc.sorel@gmail.com>
2025-01-30 18:02:40 +0100

    feat(test.client): add optional kwargs parameter so that low-level requests functions can be called with additional named arguments

--- tests/support/client.py
+++ tests/support/client.py
@@ -9,33 +9,33 @@ class Client:
     def get(self, path, headers=None, **kwargs):
         return requests.get(f"{self.root + path}", headers=headers, **kwargs)
 
-    def post_as_json(self, path, body=None, headers=None):
-        return requests.post(f"{self.root + path}", json=body, headers=headers)
+    def post_as_json(self, path, body=None, headers=None, **kwargs):
+        return requests.post(f"{self.root + path}", json=body, headers=headers, **kwargs)
 
-    def post_as_text(self, path, body=None, headers=None):
+    def post_as_text(self, path, body=None, headers=None, **kwargs):
         headers = headers or {}
         headers['Content-Type'] = 'text/plain'
-        return requests.post(f"{self.root + path}", data=body, headers=headers)
+        return requests.post(f"{self.root + path}", data=body, headers=headers, **kwargs)
 
-    def post_as_image(self, path, body=None, headers=None):
+    def post_as_image(self, path, body=None, headers=None, **kwargs):
         headers = headers or {}
         headers['Content-Type'] = 'image/png'
-        return requests.post(f"{self.root + path}", data=body, headers=headers)
+        return requests.post(f"{self.root + path}", data=body, headers=headers, **kwargs)
 
-    def post_as_file(self, path, body=None, headers=None):
-        return requests.post(f"{self.root + path}", files=body, headers=headers)
+    def post_as_file(self, path, body=None, headers=None, **kwargs):
+        return requests.post(f"{self.root + path}", files=body, headers=headers, **kwargs)
 
-    def put_as_json(self, path, body=None, headers=None):
-        return requests.put(f"{self.root + path}", json=body, headers=headers)
+    def put_as_json(self, path, body=None, headers=None, **kwargs):
+        return requests.put(f"{self.root + path}", json=body, headers=headers, **kwargs)
 
-    def put_as_text(self, path, body=None, headers=None):
-        return requests.put(f"{self.root + path}", data=body, headers=headers)
+    def put_as_text(self, path, body=None, headers=None, **kwargs):
+        return requests.put(f"{self.root + path}", data=body, headers=headers, **kwargs)
 
-    def put_as_file(self, path, body=None, headers=None):
-        return requests.put(f"{self.root + path}", files=body, headers=headers)
+    def put_as_file(self, path, body=None, headers=None, **kwargs):
+        return requests.put(f"{self.root + path}", files=body, headers=headers, **kwargs)
 
-    def put(self, path, body=None, headers=None):
-        return requests.put(f"{self.root + path}", json=body, headers=headers)
+    def put(self, path, body=None, headers=None, **kwargs):
+        return requests.put(f"{self.root + path}", json=body, headers=headers, **kwargs)
 
-    def delete(self, path, headers=None):
-        return requests.delete(f"{self.root + path}", headers=headers)
+    def delete(self, path, headers=None, **kwargs):
+        return requests.delete(f"{self.root + path}", headers=headers, **kwargs)
