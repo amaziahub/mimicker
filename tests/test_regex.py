f76d21e0e4bf7e869b08cdcc40f0576410259221
Austin Milt <austin.w.milt@gmail.com>
2025-05-04 15:28:14 -0400

    handling repeated variable names

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,5 +1,7 @@
 from re import Pattern
-from hamcrest import assert_that, equal_to, none, not_none
+import re
+from hamcrest import assert_that, equal_to, none, not_none, raises
+from mimicker.error import TemplateError
 from mimicker.regex import parse_endpoint_pattern
 
 def test_parse_endpoint_pattern_static_path_matches_static_path():
@@ -80,3 +82,38 @@ def test_parse_endpoint_pattern_with_multiple_path_params_and_query_params_match
     match = pattern.match("/some/path/hello/ignored/path/mimicker/ignored/2?age=20&city=NewYork&ignored=param")
     assert_that(match, not_none())
     assert_that(match.groupdict(), equal_to({"greeting": "hello", "name": "mimicker", "age": "20", "city": "NewYork"}))
+
+
+def test_parse_endpoint_pattern_with_repeated_query_param_names_throws_error():
+    assert_that(
+        lambda: parse_endpoint_pattern("/hello/mimicker?age={age}&city={city}&age={age}"), 
+        raises(TemplateError)
+    )
+
+
+def test_parse_endpoint_pattern_with_repeated_query_params_distinctly_named_matches():
+    pattern: Pattern = parse_endpoint_pattern("/hello/mimicker?greeting={name1}&greeting={name2}")
+    match = pattern.match("/hello/mimicker?greeting=world1&greeting=world2")
+    assert_that(match, not_none())  
+    assert_that(match.groupdict(), equal_to({"name1": "world1", "name2": "world2"}))
+
+
+def test_parse_endpoint_pattern_with_repeated_param_names_throws_error():
+    assert_that(
+        lambda: parse_endpoint_pattern("/{greeting}/mimicker?greeting={greeting}"), 
+        raises(TemplateError)
+    )
+
+
+def test_parse_endpoint_pattern_with_repeated_params_distinctly_named_matches():
+    pattern: Pattern = parse_endpoint_pattern("/{greeting1}/mimicker?greeting={greeting2}")
+    match = pattern.match("/hello/mimicker?greeting=world1")
+    assert_that(match, not_none())  
+    assert_that(match.groupdict(), equal_to({"greeting1": "hello", "greeting2": "world1"}))
+
+
+def test_parse_endpoint_pattern_with_invalid_query_param_throws_error():
+    assert_that(
+        lambda: parse_endpoint_pattern("/hello/mimicker?age={age1}&city={city}&age{age2}"), 
+        raises(TemplateError)
+    )
